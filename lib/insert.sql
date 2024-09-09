95285612e8a39acfca19c65a16b4c4e722619144
KomfortKimko <komfortkimk@gmail.com>
2024-09-09 15:10:26 +0300

    Organizing-bears-lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 10, 'M', 'brown', 'calm', 1),
+('Rowdy', 8, 'M', 'black', 'aggressive', 1),
+('Tabitha', 6, 'F', 'white', 'calm', 1),
+('Sergeant Brown', 12, 'M', 'brown', 'aggressive', 0),
+('Melissa', 7, 'F', 'black', 'playful', 1),
+('Grinch', 5, 'M', 'brown', 'grumpy', 1),
+('Wendy', 9, 'F', 'brown', 'calm', 0),
+(NULL, 4, 'F', 'black', 'curious', 1); 
